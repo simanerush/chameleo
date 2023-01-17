1b03760e4c52a7c33d052b9f3145bd88936d95b3
Sima Nerush <2002ssn@gmail.com>
2023-01-16 21:18:44 -0800

    clean up model

--- Quotes/Model/QuoteModel.swift
+++ Quotes/Model/QuoteModel.swift
@@ -8,24 +8,29 @@
 import CoreData
 
 class QuoteModel: ObservableObject {
-  let defaults = UserDefaults(suiteName: "group.com.simanerush.Quotes")!
   let persistenceController: PersistenceController
+  let defaults = UserDefaults(suiteName: "group.com.simanerush.Quotes")!
   
   init(persistenceController: PersistenceController) {
     self.persistenceController = persistenceController
   }
   
+  /// fetch today's quote
   func getTodayQuote() -> String {
     if let storedQuote = defaults.array(forKey: "todaysQuote") {
-      let storedDate = storedQuote.first! as! Date
-      if !storedDate.hasSameOfMultiple([.day, .month, .year], as: Date()) {
+      // if the date of the stored quote is different from the current date,
+      // recompute the quote
+      let storedQuoteDate = storedQuote.first! as! Date
+      if !storedQuoteDate.hasSameOfMultiple([.day, .month, .year], as: Date()) {
         self.computeRandomQuote()
       }
     } else if defaults.array(forKey: "todaysQuote") == nil {
+      // also recompute the quote if there is no quote stored
       self.computeRandomQuote()
     }
     
     if let quote = defaults.array(forKey: "todaysQuote") {
+      // finally, return quote of the day
       return quote[1] as! String
     } else {
       return "you don't have any quotes!"
@@ -33,9 +38,10 @@ class QuoteModel: ObservableObject {
   }
   
   func computeRandomQuote() {
+    let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Quote")
     var fetchedQuotes = [String]()
     
-    let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Item")
+    // fetch all quotes first
     do {
       if let results = try persistenceController.container.viewContext.fetch(fetchRequest) as? [NSManagedObject] {
         for result in results {
@@ -47,6 +53,7 @@ class QuoteModel: ObservableObject {
     } catch {
       fatalError("🚨failed to fetch data")
     }
+    
     if !fetchedQuotes.isEmpty {
       defaults.set([Date(), fetchedQuotes.randomElement() as Any], forKey: "todaysQuote")
     }
