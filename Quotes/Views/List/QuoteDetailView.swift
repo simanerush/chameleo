a099a3333a043ed6b1ca28bba27c269ab1fe92bd
Sima Nerush <2002ssn@gmail.com>
2023-02-03 10:39:29 -0800

    edit detail view

--- Quotes/Views/List/QuoteDetailView.swift
+++ Quotes/Views/List/QuoteDetailView.swift
@@ -17,23 +17,31 @@ struct QuoteDetailView: View {
         NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)
       ],
     animation: .default)
+  
   private var items: FetchedResults<Item>
   
   @ObservedObject var item: Item
   
+  @AppStorage("backgroundColor", store: UserDefaults(suiteName: "group.com.simanerush.Quotes")) private var backgroundColor = Color(UIColor(red: 0.99, green: 0.80, blue: 0.43, alpha: 1.00))
+  @AppStorage("fontColor", store: UserDefaults(suiteName: "group.com.simanerush.Quotes")) private var fontColor: Color = .white
+  
   @State private var alertIsPresented = false
   
   var body: some View {
-    TextField("edit the quote", text: Binding($item.title)!)
-      .onChange(of: item.title!) { _ in
-        do {
-          try viewContext.save()
-        } catch {
-          alertIsPresented.toggle()
+    VStack {
+      TextField("edit the quote", text: Binding($item.title)!)
+        .onChange(of: item.title!) { _ in
+          do {
+            try viewContext.save()
+          } catch {
+            alertIsPresented.toggle()
+          }
         }
-      }
-      .alert("🚨failed to edit the quote!", isPresented: $alertIsPresented) {
-        Button("ok", role: .cancel) {}
-      }
+        .textFieldStyle(.roundedBorder)
+        .padding()
+    }
+    .alert("🚨failed to edit the quote!", isPresented: $alertIsPresented) {
+      Button("ok", role: .cancel) {}
+    }
   }
 }
